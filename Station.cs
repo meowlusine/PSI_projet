ca8615ddece4e01126c3ab05fab1fd8792d18591
chloechamy <chloe.chamy@edu.devinci.fr>
2025-04-01 13:10:48 +0200

    Add files via upload

---
+++ Station.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSI
+{
+    public class Station
+    {
+        public string Nom_station { get; set; }
+        public int Ligne { get; set; }
+        public string Commune_nom { get; set; }
+        public int Commune_code { get; set; }
+        public float Longitude { get; set; }
+        public float Latitude { get; set; }
+        
+    }
+
+
+}
+
