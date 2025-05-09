5075ea7c8e3be3006b4264c15bd1ce52b2df543f
Mélusine PULIERO <96616995+meowlusine@users.noreply.github.com>
2025-05-09 09:19:46 +0200

    Update GrapheUtilisateurVisualizer.cs

--- GrapheUtilisateurVisualizer.cs
+++ GrapheUtilisateurVisualizer.cs
@@ -50,7 +50,7 @@ namespace PSI
                 var paintEdge = new SKPaint { Color = SKColors.Gray, StrokeWidth = 2, IsAntialias = true };
                 var paintText = new SKPaint { Color = SKColors.Black, TextSize = 20, IsAntialias = true };
 
-                // Dessiner les arêtes
+                
                 foreach (var utilisateur in graphe.Utilisateurs)
                 {
                     foreach (var (voisin, commande) in graphe.ObtenirCommandes(utilisateur))
@@ -62,7 +62,6 @@ namespace PSI
                     }
                 }
 
-                // Dessiner les sommets
                 foreach (var utilisateur in graphe.Utilisateurs)
                 {
                     var pos = positions[utilisateur];
